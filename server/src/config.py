f0d8fda9383a45e0417ac051cbe6c122384cab9e
pbadevel <pavelburykin13@gmail.com>
2026-09-01 19:14:03 +0300

    .

--- server/src/config.py
+++ server/src/config.py
@@ -14,7 +14,7 @@ class Environment(StrEnum):
     production = "production"
 
 
-env = Environment(os.getenv("B_ENV", Environment.development))
+env = Environment(os.getenv("ENV", Environment.development))
 env_file = ".env.testing" if env == Environment.testing else ".env"
 
 
